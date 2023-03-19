c6a0fab1305bea102a181f4e974263725a1b7336
Conner Bachmann <github@conner-bachmann.de>
2023-03-19 12:19:24 +0100

    feat: add plugin for user id

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,12 @@
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin } from '#app';
+import { v4 as uuidv4 } from 'uuid';
 
-export default defineNuxtPlugin((nuxtApp) => {
-  console.log('Plugin injected by my-module!')
-})
+export default defineNuxtPlugin(() => {
+  if (typeof window !== 'undefined') {
+    const storage = window.localStorage;
+
+    if (!storage.getItem("docus-feedback-user")) {
+      storage.setItem("fdocus-feedback-user", uuidv4());
+    }
+  }
+});
