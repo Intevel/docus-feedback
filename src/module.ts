c380d46a41a5f9fececed7d81fe0a5e576e05bf7
Conner Bachmann <github@conner-bachmann.de>
2023-03-19 12:18:46 +0100

    feat: configure module

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,75 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import {
+  defineNuxtModule,
+  addPlugin,
+  createResolver,
+  addServerHandler,
+} from "@nuxt/kit";
+import { fileURLToPath } from "url";
+import defu from "defu";
 
-// Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface DocusFeedbackOptions {
+  /**
+   * Setting to `true` gives the user an id stored in the local storage
+   * @default true
+   * @description Use this for unique user tracking.
+   */
+  autoUserTracking?: boolean;
+  /**
+   * Setting to `false` disables the module.
+   * @default true
+   * @description Use this setting to disable the module.
+   */
+  isEnabled?: boolean;
+}
 
-export default defineNuxtModule<ModuleOptions>({
+export default defineNuxtModule<DocusFeedbackOptions>({
   meta: {
-    name: 'docus-feedback',
-    configKey: 'feedback'
+    name: "docus-feedback",
+    configKey: "feedback",
+    compatibility: {
+      nuxt: "^3.0.0",
+    },
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
-  setup (options, nuxt) {
-    const resolver = createResolver(import.meta.url)
+  defaults: {
+    isEnabled: true,
+    autoUserTracking: true
+  },
+  setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url);
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    if (!options.isEnabled) {
+      // eslint-disable-next-line no-console
+      return console.warn(`[docus-feedback] module is disabled and will not be loaded.`)
+    }
+
+    const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
+
+    // Nuxt 3
+    nuxt.options.runtimeConfig.public = nuxt.options.runtimeConfig.public || {}
+    nuxt.options.runtimeConfig.public.directus = defu(nuxt.options.runtimeConfig.directus, {
+      isEnabled: options.isEnabled,
+      autoUserTracking: options.autoUserTracking
+    })
+
+    if (options.autoUserTracking) {
+      // Registers plugin for generating user ids
+      addPlugin(resolve(runtimeDir, "./plugin"));
+    }
+
+    // Add server handler
+    addServerHandler({
+      route: "/api/feedback",
+      method: "post",
+      handler: resolve(runtimeDir, "server/api/feedback.post"),
+    });
+  },
+});
+
+declare module '@nuxt/schema' {
+  interface ConfigSchema {
+    publicRuntimeConfig?: {
+      feedback?: DocusFeedbackOptions;
+    };
   }
-})
+}
